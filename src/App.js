33074ae928c0055cb5840aa54f89a436c229c5ce
Asifm95 <39342861+Asifm95@users.noreply.github.com>
2020-05-15 04:06:44 +0530

    add image filters
    add new filters to images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.scss";
 
-const images = [
-  "https://images.pexels.com/photos/3617536/pexels-photo-3617536.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-  "https://images.pexels.com/photos/3592799/pexels-photo-3592799.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-  "https://images.pexels.com/photos/3484702/pexels-photo-3484702.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-  "https://images.pexels.com/photos/3600101/pexels-photo-3600101.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-];
+const image =
+  "https://images.pexels.com/photos/3600101/pexels-photo-3600101.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
 
 const elements = [
   { className: "img1 a" },
@@ -16,17 +12,6 @@ const elements = [
 ];
 
 const App = () => {
-  const [url, setUrl] = useState(images[0]);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const idx = images.indexOf(url) + 1;
-      idx >= 0 && idx < images.length ? setUrl(images[idx]) : setUrl(images[0]);
-    }, 5000);
-    return () => {
-      clearInterval(interval);
-    };
-  });
-
   return (
     <section className="main">
       <div className="container">
@@ -35,7 +20,7 @@ const App = () => {
             <div
               key={idx}
               className={ele.className}
-              style={{ background: `50%/cover no-repeat url(${url})` }}
+              style={{ background: `50%/cover no-repeat url(${image})` }}
             />
           ))}
         </div>
